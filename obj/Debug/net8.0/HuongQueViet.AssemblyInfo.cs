76d47480bfddf4758d5f1119e1e496dbf273ed55
Khoa commit <khoahuynhkhoah12321@gmail.com>
2026-08-07 09:13:12 +0700

    khoa push code

--- obj/Debug/net8.0/HuongQueViet.AssemblyInfo.cs
+++ obj/Debug/net8.0/HuongQueViet.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuongQueViet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525a0875088b2c562c86b7ff1d87920b10ed183d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce76f7f7ee659913d95564f4f27bcfca52c9c627")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuongQueViet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuongQueViet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
