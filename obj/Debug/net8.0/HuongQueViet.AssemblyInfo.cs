f68836e872bac49ffaf50c77713078fdc62e9299
Hoang <2311552978@nttu.edu.vn>
2026-08-09 16:31:44 +0700

    git logo for Hung

--- obj/Debug/net8.0/HuongQueViet.AssemblyInfo.cs
+++ obj/Debug/net8.0/HuongQueViet.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuongQueViet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65dc62b70ba11fdf889a1111e651f857f9536ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b19d47b46234d0231e9b02c0012632908a051c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuongQueViet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuongQueViet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
